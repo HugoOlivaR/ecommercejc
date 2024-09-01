fdfb49ce3da6db4d9f045b2050de852a149f9f34
hugoolv <hugoliva158@gmail.com>
2024-09-01 16:08:19 +0200

    functional form

--- wildsphere-storefront/src/modules/layout/components/forms/mailForm.tsx
+++ wildsphere-storefront/src/modules/layout/components/forms/mailForm.tsx
@@ -1,21 +1,37 @@
-import React from "react"
+"use client"
 
-export default function mailForm() {
+import React from "react"
+import { useForm, ValidationError } from "@formspree/react"
+
+export default function MailForm() {
+  const [state, handleSubmit] = useForm("xrbzdzzp") // Asegúrate de que este ID es correcto.
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>
+  }
   return (
-    <form className="flex flex-col text-left w-full">
-      <label htmlFor="email" className="mb-2 text-ui-fg-subtle text-sm">Recive Updates:</label>
+    <form
+      method="POST"
+      onSubmit={handleSubmit}
+      className="flex flex-col text-left w-full"
+    >
+      <label htmlFor="email" className="mb-2 text-ui-fg-subtle text-sm">
+        Recive Updates:
+      </label>
       <input
         type="email"
+        name="email"
         title="Enter Email"
         placeholder="Email Address"
         className="border-b border-black p-2"
         required
       />
+      <ValidationError prefix="Email" field="email" errors={state.errors} />
       <div className="w-full flex justify-end">
         <button
           type="submit"
           title="Submit Email"
           className="w-fit text-sm p-2"
+          disabled={state.submitting}
         >
           Submit
         </button>
